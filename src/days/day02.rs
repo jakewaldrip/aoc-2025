3f2734f5683b81787220992ce91856c84a690f33
Jacob Waldrip <jakeywaldrip@gmail.com>
2025-11-30 23:03:32 -0600

    Initial commit

---
+++ src/days/day02.rs
@@ -0,0 +1,23 @@
+use crate::{Solution, SolutionPair};
+
+///////////////////////////////////////////////////////////////////////////////
+
+pub fn solve(input: &str) -> SolutionPair {
+    println!("Input: \n{input}");
+    let sol1 = 0;
+    let sol2 = 0;
+    (Solution::from(sol1), Solution::from(sol2))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_input_day5_p1() {
+        let input = "";
+        let (p1, _) = solve(input);
+        let p1_result = format!("{p1}");
+        assert_eq!(p1_result, "0");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,66 @@
+mod days;
+mod utils;
+
+use days::{day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12};
+use std::env;
+use std::fs::read_to_string;
+use std::time::Instant;
+use utils::solution::Solution;
+
+pub type SolutionPair = (Solution, Solution);
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Please provide the day(s) to run as a command-line argument.");
+    }
+
+    let days: Vec<u8> = args[1..]
+        .iter()
+        .map(|x| {
+            x.parse()
+                .unwrap_or_else(|v| panic!("Not a valid day: {}", v))
+        })
+        .collect();
+
+    let mut runtime = 0.0;
+
+    for day in days {
+        let func = get_day_solver(day);
+
+        let time = Instant::now();
+
+        let input_path = format!("inputs/day{}.txt", day);
+        let input = read_to_string(input_path).expect("Failed to read input file");
+
+        let (p1, p2) = func(&input);
+        let elapsed_ms = time.elapsed().as_nanos() as f64 / 1_000_000.0;
+
+        println!("\n=== Day {:02} ===", day);
+        println!("  · Part 1: {}", p1);
+        println!("  · Part 2: {}", p2);
+        println!("  · Elapsed: {:.4} ms", elapsed_ms);
+
+        runtime += elapsed_ms;
+    }
+
+    println!("Total runtime: {:.4} ms", runtime);
+}
+
+fn get_day_solver(day: u8) -> fn(&str) -> SolutionPair {
+    match day {
+        1 => day01::solve,
+        2 => day02::solve,
+        3 => day03::solve,
+        4 => day04::solve,
+        5 => day05::solve,
+        6 => day06::solve,
+        7 => day07::solve,
+        8 => day08::solve,
+        9 => day09::solve,
+        10 => day10::solve,
+        11 => day11::solve,
+        12 => day12::solve,
+        _ => unimplemented!(),
+    }
+}

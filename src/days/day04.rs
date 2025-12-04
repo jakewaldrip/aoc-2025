c7a27c6ab50a7d811a9700bb12b76ca26643dbf6
Jacob Waldrip <jakeywaldrip@gmail.com>
2025-12-04 01:15:20 -0600

    Day 4 ready to go

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,9 +1,10 @@
-use crate::{Solution, SolutionPair};
+use crate::{Solution, SolutionPair, utils::grid::Grid};
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve(input: &str) -> SolutionPair {
-    println!("Input: \n{input}");
+    let grid = Grid::new(input);
+    println!("{grid}");
     let sol1 = 0;
     let sol2 = 0;
     (Solution::from(sol1), Solution::from(sol2))
@@ -14,10 +15,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example_input_day5_p1() {
-        let input = "";
+    fn test_example_input_day4_p1() {
+        let input = "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@.";
         let (p1, _) = solve(input);
         let p1_result = format!("{p1}");
-        assert_eq!(p1_result, "0");
+        assert_eq!(p1_result, "13");
+    }
+
+    #[test]
+    fn test_example_input_day4_p2() {
+        let input = "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@.";
+        let (_, p2) = solve(input);
+        let p2_result = format!("{p2}");
+        assert_eq!(p2_result, "0");
     }
 }
